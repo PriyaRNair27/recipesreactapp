7ea1385a310336c04deda765b648c078d20f769d
PriyaRNair27 <priyarnair2710@gmail.com>
2022-05-28 14:15:35 +0530

    search

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import Addrecipes from './Components/Addrecipes';
+import Searchrecipes from './Components/Searchrecipes';
 
 function App() {
   return (
     <div>
 <Addrecipes/>
+<Searchrecipes/>
 
 
     </div>

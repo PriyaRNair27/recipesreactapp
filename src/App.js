e59d1d5e614a04e5a21116b6153e940b9568e826
PriyaRNair27 <priyarnair2710@gmail.com>
2022-05-28 14:31:07 +0530

    nav

--- src/App.js
+++ src/App.js
@@ -4,12 +4,18 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import Addrecipes from './Components/Addrecipes';
 import Searchrecipes from './Components/Searchrecipes';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div>
-<Addrecipes/>
-<Searchrecipes/>
+<BrowserRouter>
+<Routes>
+  <Route path="/" exact element={<Addrecipes/>}/>
+  <Route path="/search" exact element={<Searchrecipes/>}/>
+  
+</Routes>
+</BrowserRouter>
 
 
     </div>
--- src/Components/Addrecipes.js
+++ src/Components/Addrecipes.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import Header from './Header'
 
 const Addrecipes = () => {
     var [recipetitle,setrecipetitle]=useState("")
@@ -11,7 +12,9 @@ const Addrecipes = () => {
     }
 
   return (
-    <div><div className="container">
+    <div>
+        <Header/>
+        <div className="container">
     <div className="row">
         <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
             <div className="row g-3">
